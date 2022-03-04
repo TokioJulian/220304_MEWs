3485fcea0aaa4e4f537ea40e422941513d9c235b
TokioJulian <thethirdmedergist@gmail.com>
2022-03-04 17:15:05 +0900

    Update secret_key.py

--- MEWs01/secret_key.py
+++ MEWs01/secret_key.py
@@ -1,5 +1,7 @@
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = get_random_secret_key()
-# text = 'SECRET_KEY = \'{0}\''.format(secret_key)
+# SECRET_KEY = get_random_secret_key()
+# text = 'SECRET_KEY = \'{0}\''.format(SECRET_KEY)
 # print(text)
+
+SECRET_KEY = '6lpa@=6732_&z9g(wdyd+hh+gifz#-k3%9h2&!s2@cr1-hia*5'
